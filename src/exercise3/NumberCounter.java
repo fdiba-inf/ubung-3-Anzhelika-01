02097e8d817dd093bc74057c1b40dd7d29bfcc6f
Anzhelika01 <anzi303@abv.bg>
2020-10-14 12:34:22 +0000

    .

---
+++ src/exercise3/NumberCounter.java
@@ -0,0 +1,35 @@
+package exercise3;
+import java.util.Scanner;
+  
+  public class NumberCounter{
+    public static void main(String[] args){
+
+      Scanner input = new Scanner(System.in);
+      double num;
+      int positiveNumbers = 0;
+      int negativeNumbers = 0;
+      double sum = 0.0;
+      double average = 0.0;
+      int count = 0;
+
+        num = input.nextDouble();
+      while(num != 0)
+      {
+        count++;
+        if(num % 2 == 0)
+        {
+          positiveNumbers++;
+        }
+        else
+        {
+          negativeNumbers++;
+        }
+        sum += num;
+      }
+      average = sum/count;
+      System.out.println("Positive numbers: " + positiveNumbers);
+      System.out.println("Negative numbers: " + negativeNumbers);
+      System.out.println("Sum: " + sum);
+      System.out.println("Average: " + average);
+    }
+  }
