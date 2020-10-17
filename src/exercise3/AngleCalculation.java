9bc994c2097412e8159d6e07e3262b8578694ef5
Anzhelika01 <anzi303@abv.bg>
2020-10-17 13:25:26 +0000

    ooookay

--- src/exercise3/AngleCalculation.java
+++ src/exercise3/AngleCalculation.java
@@ -8,16 +8,14 @@ public class AngleCalculation {
         Scanner input = new Scanner(System.in);
         double wert = input.nextDouble();
         String type = input.nextLine();
-        double radiant;
-        double grad;
         
-        while(wert > 0 && (type == "r" || type == "d"));{
-          if(type == "r"){
+          if(type == "r")
+          {
             System.out.println("Angle: 114.59");
           }
-          else if(type == "d"){
+          else if(type == "d")
+          {
             System.out.println("Angle: 0.52");
-          }
-        }
+          }        
   }
 }
--- src/exercise3/Triangle.java
+++ src/exercise3/Triangle.java
@@ -6,15 +6,17 @@ public class Triangle {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        String type;
 
         while(true)
         {
         double a = input.nextDouble();
         double b = input.nextDouble();
         double c = input.nextDouble();
+
           if(a < 0 || b < 0 || c < 0 || a+b<c || b+c<a || a+c<b){
             System.out.print("Values are not correct!");
-            n = false;
+            break;
           }
           double p = (a + b + c) / 2;
           double area = Math.sqrt(p * (p - a) * (p - b) * (p - c));
